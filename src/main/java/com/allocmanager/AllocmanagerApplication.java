de001d112a39d458a3bf05f2a64a9647a3b6c0ee
Nelson Vieira Cunha Lima <nelson.lima@mv.com>
2026-02-24 18:04:22 -0300

    primeiro commit

---
+++ src/main/java/com/allocmanager/AllocmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.allocmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllocmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllocmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allocmanager/AllocmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allocmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllocmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
